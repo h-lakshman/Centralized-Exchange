6b19ed5e77f29f0743471881f700e4cd6fd4e51b
h-lakshman <lakshman.vaibhav007@gmail.com>
2025-05-14 22:36:44 +0530

    /order routes

--- api/src/handlers/order.rs
+++ api/src/handlers/order.rs
@@ -1,35 +1,62 @@
+use crate::{
+    redis_manager::RedisManager,
+    types::{
+        CancelOrderRequest, EngineMessageData, GetOpenOrdersRequest, MessageToEngine,
+        MessageToType, PlaceOrderRequest,
+    },
+};
 use actix_web::{web, HttpResponse, Responder};
-use crate::types::{PlaceOrderRequest, MessageToEngine, MessageToType, EngineMessageData, CancelOrderRequest, GetOpenOrdersRequest};
 
-pub async fn create_order(
-    data: web::Json<PlaceOrderRequest>,
-) -> impl Responder {
-   let message_to_engine = MessageToEngine {
-    message_type: MessageToType::PlaceOrder,
-    data: EngineMessageData::PlaceOrder(data.into_inner()),
-   };
-   //todo: send message to engine
-   HttpResponse::Ok().json(message_to_engine)
+pub async fn create_order(data: web::Json<PlaceOrderRequest>) -> impl Responder {
+    let message_to_engine = MessageToEngine {
+        message_type: MessageToType::PlaceOrder,
+        data: EngineMessageData::PlaceOrder(data.into_inner()),
+    };
+
+    let redis_manager = RedisManager::get_instance();
+    match redis_manager.lock() {
+        Ok(manager) => match manager.send_and_await(message_to_engine).await {
+            Ok(response) => HttpResponse::Ok().json(response),
+            Err(e) => HttpResponse::InternalServerError().body(format!("Error: {}", e)),
+        },
+        Err(e) => {
+            HttpResponse::InternalServerError().body(format!("Redis manager lock error: {}", e))
+        }
+    }
 }
 
-pub async fn cancel_order(
-    data: web::Json<CancelOrderRequest>,
-) -> impl Responder {
+pub async fn cancel_order(data: web::Json<CancelOrderRequest>) -> impl Responder {
     let message_to_engine = MessageToEngine {
         message_type: MessageToType::CancelOrder,
         data: EngineMessageData::CancelOrder(data.into_inner()),
     };
-    //todo: send message to engine
-    HttpResponse::Ok().json(message_to_engine)
+
+    let redis_manager = RedisManager::get_instance();
+    match redis_manager.lock() {
+        Ok(manager) => match manager.send_and_await(message_to_engine).await {
+            Ok(response) => HttpResponse::Ok().json(response),
+            Err(e) => HttpResponse::InternalServerError().body(format!("Error: {}", e)),
+        },
+        Err(e) => {
+            HttpResponse::InternalServerError().body(format!("Redis manager lock error: {}", e))
+        }
+    }
 }
 
-pub async fn get_open_orders(
-    data: web::Json<GetOpenOrdersRequest>,
-) -> impl Responder {
+pub async fn get_open_orders(data: web::Json<GetOpenOrdersRequest>) -> impl Responder {
     let message_to_engine = MessageToEngine {
         message_type: MessageToType::GetOpenOrders,
         data: EngineMessageData::GetOpenOrders(data.into_inner()),
     };
-    //todo: send message to engine
-    HttpResponse::Ok().json(message_to_engine)
+
+    let redis_manager = RedisManager::get_instance();
+    match redis_manager.lock() {
+        Ok(manager) => match manager.send_and_await(message_to_engine).await {
+            Ok(response) => HttpResponse::Ok().json(response),
+            Err(e) => HttpResponse::InternalServerError().body(format!("Error: {}", e)),
+        },
+        Err(e) => {
+            HttpResponse::InternalServerError().body(format!("Redis manager lock error: {}", e))
+        }
+    }
 }
