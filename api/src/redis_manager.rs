1661f812b69a318ddea821a8aaf8d79c14d0f216
h-lakshman <lakshman.vaibhav007@gmail.com>
2025-05-14 22:36:26 +0530

    async redis manager

--- api/src/redis_manager.rs
+++ api/src/redis_manager.rs
@@ -1,60 +1,61 @@
-use redis::{Client, Connection, PubSubCommands};
-use serde::Serialize;
+use futures_util::StreamExt;
+use redis::{AsyncCommands, Client};
 use std::error::Error;
-use std::sync::{Mutex, OnceLock};
+use std::sync::{Arc, Mutex, OnceLock};
 
 use crate::types::{MessageFromOrderbook, MessageToEngine};
 
 pub struct RedisManager {
-    client_conn: Connection,
-    publisher_conn: Connection,
+    client: Client,
+    publisher: Client,
 }
 
-pub static REDIS_MANAGER: OnceLock<Mutex<RedisManager>> = OnceLock::new();
+pub static REDIS_MANAGER: OnceLock<Arc<Mutex<RedisManager>>> = OnceLock::new();
 
 impl RedisManager {
     fn new() -> Result<Self, Box<dyn Error>> {
         let client = Client::open("redis://127.0.0.1:6379")?;
         let publisher = Client::open("redis://127.0.0.1:6379")?;
 
-        let client_conn = client.get_connection()?;
-        let publisher_conn = publisher.get_connection()?;
-
-        Ok(RedisManager {
-            client_conn,
-            publisher_conn,
-        })
+        Ok(RedisManager { client, publisher })
     }
 
-    pub fn get_instance() -> &'static Mutex<RedisManager> {
+    pub fn get_instance() -> &'static Arc<Mutex<RedisManager>> {
         REDIS_MANAGER.get_or_init(|| {
-            Mutex::new(
-                RedisManager::new()
-                    .expect("Failed to create RedisManager instance or instance already exists"),
-            )
+            Arc::new(Mutex::new(RedisManager::new().expect(
+                "Failed to create RedisManager instance or instance already exists",
+            )))
         })
     }
 
     pub async fn send_and_await(
-        &mut self,
+        &self,
         message: MessageToEngine,
     ) -> Result<MessageFromOrderbook, Box<dyn Error>> {
+        // async connectors
+        let client_conn = self.client.get_async_connection().await?;
+        let mut publisher_conn = self.publisher.get_async_connection().await?;
+
         let client_id = self.get_random_client_id();
+
+        // Set up pubsub
+        let mut pubsub = client_conn.into_pubsub();
+        pubsub.subscribe(&client_id).await?;
+        let mut pubsub_stream = pubsub.on_message();
+
         let payload = serde_json::to_string(&(client_id.clone(), message))?;
-        let mut pubsub = self.client_conn.as_pubsub();
-        pubsub
-            .subscribe(&client_id)
-            .expect("Failed to subscribe to client ID");
-        redis::cmd("LPUSH")
-            .arg("messages")
-            .arg(&payload)
-            .execute(&mut self.publisher_conn);
 
-        let msg = pubsub.get_message()?;
-        let response: String = msg.get_payload()?;
-        let result: MessageFromOrderbook = serde_json::from_str(&response)?;
+        //push to queue
+        let _: () = publisher_conn.lpush("messages", payload).await?;
 
-        Ok(result)
+        // Wait for response
+        if let Some(msg) = pubsub_stream.next().await {
+            let response: String = msg.get_payload()?;
+            let result: MessageFromOrderbook = serde_json::from_str(&response)?;
+            Ok(result)
+        } else {
+            Err("No message received".into())
+        }
     }
 
     pub fn get_random_client_id(&self) -> String {
