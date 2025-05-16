519eb1e76056b00e84f14fc61d927c181b032d08
h-lakshman <lakshman.vaibhav007@gmail.com>
2025-05-16 12:19:07 +0530

    Enhance Redis message handling: integrated serde for serialization, added DbMessage struct, and implemented push_message and send_to_api methods in redis_manager.

--- engine/src/redis_manager.rs
+++ engine/src/redis_manager.rs
@@ -1,18 +1,22 @@
+use crate::types::DbMessageType;
+use redis::{Client, Commands, Connection};
+use serde::{Deserialize, Serialize};
 use std::{env, error::Error, sync::OnceLock};
 
-use crate::types::DbMessageType;
-use redis::Client;
-
-struct DbMessage {
+#[derive(Serialize, Deserialize)]
+pub struct DbMessage {
+    #[serde(rename = "type")]
     db_message_type: DbMessageType,
     data: DbMessageData,
 }
 
+#[derive(Serialize, Deserialize)]
 enum DbMessageData {
     TradeAdd(TradeAdd),
     OrderUpdate(OrderUpdate),
 }
 
+#[derive(Serialize, Deserialize)]
 struct TradeAdd {
     id: String,
     is_buyer_maker: bool,
@@ -22,15 +26,22 @@ struct TradeAdd {
     timestamp: String,
 }
 
+#[derive(Serialize, Deserialize)]
 struct OrderUpdate {
     order_id: String,
     executed_quantity: u64,
     price: Option<String>,
     market: Option<String>,
     quantity: Option<String>,
-    side: Option<String>,
+    side: Option<Side>,
 }
 
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+enum Side {
+    Buy,
+    Sell,
+}
 pub static REDIS_MANAGER: OnceLock<RedisManager> = OnceLock::new();
 pub struct RedisManager {
     client: Client,
@@ -48,4 +59,20 @@ impl RedisManager {
                 .expect("Failed to create RedisManager instance or REDIS_URL is not set")
         })
     }
+
+    pub fn push_message(&self, message: DbMessage) -> Result<(), Box<dyn Error>> {
+        let mut connection = self.client.get_connection()?;
+        let payload = serde_json::to_string(&message)?;
+        let _: () = connection.lpush("db_processor", payload)?;
+        Ok(())
+    }
+
+    pub fn send_to_api(&self, channel: &str, message: DbMessage) -> Result<(), Box<dyn Error>> {
+        let mut connection = self.client.get_connection()?;
+        let payload = serde_json::to_string(&message)?;
+        let _: () = connection.publish(channel, payload)?;
+        Ok(())
+    }
+
+    //
 }
--- engine/src/types.rs
+++ engine/src/types.rs
@@ -1,4 +1,16 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
 pub enum DbMessageType {
+    TradeAdded,
+    OrderCreated,
+}
+pub const TRADE_ADDED: &str = "TRADE_ADDED";
+pub const ORDER_CREATED: &str = "ORDER_CREATED";
+
+//Send To Api Types
+enum SendToApiType {
     TradeAdded,
     OrderCreated,
 }
